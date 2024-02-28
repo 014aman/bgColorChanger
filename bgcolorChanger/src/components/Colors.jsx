c091a7126c2c676da2eb3a194785496605e5436c
“14AMAN” <amannimcet2021@gmail.com>
2024-02-28 12:17:38 +0530

    adding propsValidation

--- bgcolorChanger/src/components/Colors.jsx
+++ bgcolorChanger/src/components/Colors.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Colors({ colorName, color, setColor }) {
   const handleClick = () => {
-    setColor((colorName.toLowerCase()));
+    setColor(colorName.toLowerCase());
   };
   return (
-    <div className={`w-20 text-center rounded-lg ${color}`} onClick={handleClick}>{colorName}</div>
+    <div
+      className={`w-20 text-center rounded-lg ${color}`}
+      onClick={handleClick}
+    >
+      {colorName}
+    </div>
   );
 }
+
+Colors.propTypes = {
+  colorName: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  setColor: PropTypes.func.isRequired,
+};
+
 export default Colors;
